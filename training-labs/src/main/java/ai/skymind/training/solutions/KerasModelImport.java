6c2010d409a121ba1bd304bd4101f796f01614cb
tomthetrainer <tom@functionalmedia.com>
2017-03-13 02:17:12 -0700

    Changed some stuff

--- training-labs/src/main/java/ai/skymind/training/solutions/KerasModelImport.java
+++ training-labs/src/main/java/ai/skymind/training/solutions/KerasModelImport.java
@@ -4,6 +4,7 @@ import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+//import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 
 
 /*
